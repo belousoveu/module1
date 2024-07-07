6ad0efc4bf962be7df6df796e62f5cb9e5cecb38
Belousoveu <belousov.eu@gmail.com>
2024-07-07 22:23:28 +0300

    Initial commit

---
+++ homework1.py
@@ -0,0 +1,12 @@
+# step1
+example = "Топинамбур"
+# step2
+print(example[0])
+# step3
+print(example[-1])
+# step4
+print(example[len(example) // 2:])
+# step5
+print(example[::-1])
+# step6
+print(example[1::2])
