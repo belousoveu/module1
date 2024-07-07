616822ea5116e1d962684577f2417859b08f35d4
Belousoveu <belousov.eu@gmail.com>
2024-07-07 22:29:44 +0300

    complete

---
+++ homework2.py
@@ -0,0 +1,10 @@
+number_of_tasks = 12
+number_of_hours = 1.5
+course_name = "Python"
+time_per_task = number_of_hours / number_of_tasks
+# option1
+print("Курс:", course_name, ", всего задач: ", number_of_tasks, ", затрачено часов:", number_of_hours,
+      ", среднее время выполнения: ", time_per_task, )
+# option2
+print("Курс: " + course_name + ", всего задач: " + str(number_of_tasks) + ", затрачено часов:" + str(
+    number_of_hours) + ", среднее время выполнения: " + str(time_per_task))
